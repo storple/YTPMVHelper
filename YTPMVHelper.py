be28fad45f597374e166f045f7d5031ef1355157
Matthew Nguyen <158227944+storple@users.noreply.github.com>
2024-09-28 19:05:05 -0400

    Update YTPMVHelper.py

--- YTPMVHelper.py
+++ YTPMVHelper.py
@@ -1,19 +1,28 @@
 import opentimelineio as otio
 import os
 import mido
-import dotenv
+import argparse
 
-dotenv.load_dotenv()
+parser = argparse.ArgumentParser(description="YTPMV Helper Program")
+parser.add_argument("--midi", type=str, help="Path to the MIDI file")
+parser.add_argument("--video", type=str, help="Path to the video file")
+parser.add_argument("--bpm", type=float, help="Beats per minute")
+parser.add_argument("--tpb", type=float, help="Ticks per beat")
+parser.add_argument("--frame-rate", type=float, help="Frame rate of the video")
+parser.add_argument("--output", type=str, help="Output filename for the OTIO file", default="Timeline.otio")
 
-tpb = float(os.getenv("TPB"))
-bpm = float(os.getenv("BPM"))
-frame_rate = float(os.getenv("FRAME_RATE"))
-midi_file_path = os.getenv("MIDI_FILEPATH") 
-video_file_path = os.getenv("VIDEO_FILEPATH")
-otio_path = os.getenv("OUTPUT_FILENAME")
+args = parser.parse_args()
 
-midi_file_path = midi_file_path.replace("\\", "/")
-video_file_path = video_file_path.replace("\\", "/")
+tpb = args.tpb
+bpm = args.bpm
+frame_rate = args.frame_rate
+midi_file_path = args.midi
+video_file_path = args.video
+otio_path = args.output
+
+midi_file_path = os.path.normpath(midi_file_path)
+video_file_path = os.path.normpath(video_file_path)
+if not otio_path.endswith(".otio"): otio_path += ".otio"
 
 if not os.path.exists(midi_file_path):
     raise FileNotFoundError(f"Midi file not found: {midi_file_path}")
@@ -23,14 +32,17 @@ if not os.path.exists(video_file_path):
 def extract_note_start_times(midi_file_path):
     midi_file = mido.MidiFile(midi_file_path)
     note_start_times = []
-
     current_time = 0
+    last_added_time = None  
     for track in midi_file.tracks:
         for msg in track:
             current_time += msg.time
             if msg.type == 'note_on' and msg.velocity > 0:
                 converted_time = midi_ticks_to_frames(current_time, tpb, bpm)
-                note_start_times.append(converted_time)
+
+                if converted_time != last_added_time:
+                    note_start_times.append(converted_time)
+                    last_added_time = converted_time 
 
     return note_start_times
 
@@ -38,8 +50,10 @@ def midi_ticks_to_frames(ticks, tpb, bpm):
     frames = (ticks * frame_rate * 60) / (tpb * bpm)
     return int(round(frames))
 
+
 midi_start_times = extract_note_start_times(midi_file_path)
 print("Note start times saved.")
+
 
 timeline = otio.schema.Timeline("Temporary Timeline")
 video_track = otio.schema.Track(kind=otio.schema.TrackKind.Video)
